b8b0e019bc2ee1a11c32770082ac09c859860830
dridk <sacha@labsquare.org>
2013-08-01 00:45:59 +0200

    start new api design

--- mafreebox.h
+++ mafreebox.h
@@ -3,15 +3,63 @@
 
 #include <QObject>
 #include <QtNetwork>
-class MaFreeBox : public QObject
+
+
+
+struct ApiServer
+{
+    QString deviceName;
+    QString version;
+    QString baseUrl;
+    QString deviceType;
+    QString mHostName;
+    int port;
+};
+
+class MaFreeBox : public QNetworkAccessManager
 {
     Q_OBJECT
 public:
     explicit MaFreeBox(QObject *parent = 0);
 
-    void setHostName(const QString& host) ;
+    //set
+    void setHostName(const QString& host, int port = 80) ;
     void setPort(int port) ;
-    const QString& appToken() const;
+    void setApplicationToken(const QString& token);
+    bool saveApplicationToken();
+    bool loadApplicationToken(const QString& appId);
+
+    //get
+    const QString& hostName() const;
+    int port() const;
+    const QString& applicationToken() const;
+    const QString& sessionToken() const;
+    const QString& version() const;
+    const QString& challenge() const;
+    const QString& baseUrl() const;
+    const QString& challenge() const;
+
+    //request
+    void requestVersion(bool asynchronous = false);
+    void requestAuthorize(const QString& appId, const QString& appName,
+                          const QString& appVersion, const QString& deviceName, bool asynchronous = false);
+    void requestAuthorizeStatus(int trackId);
+    void requestLogin(bool asynchronous = false);
+    void requestSession(bool asynchronous = false);
+
+
+
+signals:
+    void error();
+    void sessionExpired();
+
+
+
+protected:
+    void setBaseUrl(const QString& base);
+    void setVersion(const QString& version);
+    void setSessionToken(const QString& token);
+    void setChallenge(const QString& challenge);
 
     //request methods
 public slots:
@@ -42,22 +90,18 @@ protected slots:
     void sessionFinished();
 
 protected:
-     QNetworkRequest createRequest(const QString& uri) const;
+    QNetworkRequest createRequest(const QString& uri) const;
 
 private:
     QString mHost;
     int mPort;
-    QString mBaseUrl;
+    QString mApiBaseUrl;
     QString mApiVersion;
-    QString mDeviceType;
-    QString mDeviceName;
+    QString mApplicationToken;
     QString mSessionToken;
-
-    QString mAppToken;
-    double mTrackId;
     QString mChallenge;
-    QString mAuthorizeStatus;
-    QNetworkAccessManager * mNetManager;
+
+
     
 };
 
